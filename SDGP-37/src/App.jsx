f145d4e361bb4672f6a5a316ebce6aa27fccf754
Dahamni Nisidini <dahamni.hci@gmail.com>
2024-02-02 19:17:00 +0530

    Completed the NavBar component

---
+++ SDGP-37/src/App.jsx
@@ -0,0 +1,31 @@
+
+import './App.css';
+// import Description from './components/Description/Description';
+import NavBar from './components/navBar/navBar';
+
+function App() {
+ 
+
+  return (
+  
+    <div className='app'>
+       <div className='navBar'>
+       <NavBar/>
+       </div>
+      
+      <div>
+      <h1 className='topic'>VEHICLE DAMAGE DETECTION <br></br>COST ESTIMATION</h1>
+      </div>
+
+      <div>
+      {/* <Description/> */}
+      </div>
+      
+    </div>
+   
+     
+    
+  )
+}
+
+export default App
