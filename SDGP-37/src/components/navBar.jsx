044e5908af5d397922f226740fd3061ba129f42a
Nipuna-Gamage <147525162+Nipuna-Gamage@users.noreply.github.com>
2024-02-07 20:35:08 +0530

    Update Navigation bar

--- SDGP-37/src/components/navBar.jsx
+++ SDGP-37/src/components/navBar.jsx
@@ -1,30 +1,23 @@
+import './navBar.css';
 export default function Navbar() {
   const inactiveLink = "flex gap-1 p-1";
   const activeLink = inactiveLink + " bg-white text-gray-800 rounded-l-lg";
   // const { pathname } = "/";
 
   return (
-    <nav className="flex bg-gray-700 p-6 justify-between gap-1 items-stretch">
+    <nav className="navbar">
       {/* <div className=""> */}
-      <div className="text-white text-3xl">FIXMATIC</div>
+      <div className="div1">
+        <a href="/Home" className="navbar-brand">FIXMATIC</a>
+      </div>
       {/* <div className="flex gap-6 text-xl text-white pl-[60%] flex-wrap"> */}
-      <ul className="flex p-0 m-0 text-white gap-4">
-        <li>
-          <a href="/home" className="cursor-pointer">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="/app" className="cursor-pointer h-full content-center p-1">
-            App
-          </a>
-        </li>
-        <li>
-          <a href="/contact" className="cursor-pointer">
-            Contact us
-          </a>
-        </li>
-      </ul>
+      <div className='div2'>
+        <ul className="nav-items">
+          <li><a href="/home" >Home</a></li>
+          <li><a href="/app" >App</a></li>
+          <li><a href="/contact" >Contact us</a></li>
+        </ul>
+      </div>
       {/* </div> */}
       {/* </div> */}
     </nav>
