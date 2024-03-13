e79386e9908e00f71cbef6ebca348891a5e60d44
Nipuna-Gamage <147525162+Nipuna-Gamage@users.noreply.github.com>
2024-03-13 11:46:51 +0530

    updated appPage

--- SDGP-37/src/pages/appPage.jsx
+++ SDGP-37/src/pages/appPage.jsx
@@ -21,7 +21,6 @@ export default function AppPage() {
   const HandleImageClick = () => {
     inputRef.current.click();
   };
-
   const HandleImageChange = (event) => {
     const file = event.target.files[0];
   
@@ -53,7 +52,6 @@ export default function AppPage() {
   return (
     <>
       <div>
-        <h2>Fetched</h2>
         <p>{JSON.stringify(members)}</p>
       </div>
       <div className="font-abc">
@@ -63,15 +61,13 @@ export default function AppPage() {
           </h2>
           <p className="lg:text-2xl lg:pt-8 text-left leading-9 text-white pl-14 pt-4 text-base 2xl:text-lg xl:text-lg">
             Some descriptive text explaining this guideline.
-          </p>
+            </p>
           {/* ... more guidelines */}
         </div>
         <div className="sm:ml-10 sm:mt-8 2xl:flex mt-10 xl:flex xl:mt-0">
-          {/* flex w-4/5 mt-5 ml-40 */}
           <section
             className="bg-[#37383A] w-10/12 h-56 ml-8 rounded-xl sm:w-10/12 sm:h-64  md:h-80 md:w-10/12  lg:w-5/6 lg:mt-10 xl:h-80  xl:w-6/12 2xl:w-2/5 2xl:h-72 sm:ml-0 md:min-h-28 2xl:mt-8"
-            onClick={HandleImageClick}
-          >
+            onClick={HandleImageClick}>
             {image ? (
           <img src={image} alt="Uploaded" className="w-full h-full object-cover rounded-xl" />
         ) : (
@@ -84,7 +80,6 @@ export default function AppPage() {
               className="hidden"
               ref={inputRef}
               onChange={HandleImageChange}>
-
               </input>
                 <h2 className="font-bold text-2xl text-center leading-9 text-white pl-5 pt-12 xl:pt-24">
                   Drop your image here
@@ -99,47 +94,25 @@ export default function AppPage() {
             )}
           </section>
           <section className="sm:w-5/5 mr-8 justify-between 2xl:pl-48 xl:flex xl:ml-10 ">
-            {/* <section className='sm:w-5/5 mr-8 justify-between '> */}
-            <section className=" mt-8 ml-8  text-black sm:ml-0 2xl:mt-0">
-              {/* <section className=" mb-2.5 text-black"> */}
-              <h2 className="text-white sm:ml-4 sm:mt-10  md:mt-8 sm:text-xl ">
-                Select brand
-              </h2>
-              {/* <h2 className="text-white ml-80">Select brand</h2> */}
-              <select
-                name="brand"
-                className="sm:ml-4 sm:mt-4 w-32 p-2.5 border border-solid border-white rounded-3xl text-xl bg-gray-500 mt-2"
-              >
-                <option value="Toyota">Toyota</option>
-                <option value="Toyota1">Toyota1</option>
-                <option value="Toyota2">Toyota2</option>
+            <div>
+              <select name="brand" className="sm:ml-4 sm:mt-4 w-33 p-2.5 border border-solid border-white rounded-3xl text-xl bg-gray-500 mt-2">
+                <option value="Toyota">Select brand</option>
+                <option value="Toyota1">Toyota</option>
+                <option value="Toyota2">Toyota1</option>
               </select>
-              <h2 className="text-white sm:ml-4 sm:mt-4 sm:text-xl mt-8">
-                Select type
-              </h2>
-              <div className="flex ">
-                <div>
-                  <select
-                    name="type"
-                    className=" sm:ml-4 sm:mt-4 sm:text-xl w-32 p-2.5 border border-solid border-white rounded-3xl text-xl bg-gray-500 mt-2"
-                  >
-                    <option value="Car">Car</option>
-                    <option value="Car1">Car1</option>
-                    <option value="Car2">Car2</option>
+                  <select name="type"
+                    className=" sm:ml-4 sm:mt-4 sm:text-xl w-33 p-2.5 border border-solid border-white rounded-3xl text-xl bg-gray-500 mt-2" >
+                    <option value="Car">Select type</option>
+                    <option value="Car1">Car</option>
+                    <option value="Car2">Car1</option>
                   </select>
-                </div>
-                <div>
                   <a href="/estmatedCost">
                     <button className="sm:ml-28 sm:mt-6 w-32 p-2.5 border-white rounded-3xl text-xl bg-gray-500 mt-4 ml-8">
                       Proceed
                     </button>
                   </a>
                 </div>
-              </div>
-            </section>
-
-            {/* <a href="/estmatedCost"><button className="w-32 p-2.5 border-white rounded-3xl text-xl bg-gray-500 ml-80 mt-4">Proceed</button></a> */}
-          </section>
+         </section>
         </div>
       </div>
     </>
